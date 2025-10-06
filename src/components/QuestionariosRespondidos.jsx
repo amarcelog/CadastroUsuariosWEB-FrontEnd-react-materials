16f2a34edaeb684fea2d791454a667d77e452d99
Marcelo Gouveia <151099068+amarcelog@users.noreply.github.com>
2025-10-06 12:27:47 -0300

    Update QuestionariosRespondidos.jsx

--- src/components/QuestionariosRespondidos.jsx
+++ src/components/QuestionariosRespondidos.jsx
@@ -4,7 +4,7 @@ const QuestionariosRespondidos = () => {
   return (
     <div>
       <h2>Questionários Respondidos</h2>
-      {/* Adicione aqui a lógica para exibir questionários respondidos */}
+      {/* lógica para exibir questionários respondidos */}
     </div>
   );
 };
